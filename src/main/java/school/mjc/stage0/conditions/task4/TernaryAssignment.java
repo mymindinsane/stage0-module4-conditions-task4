22f8b8c544a1e52bd03863aee1e3036ab2a1a53e
mymindinsane <arturguletskiy21@gmail.com>
2024-05-15 19:23:30 +0300

    done TernaryAssignment.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,10 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
-        int biggest = 0;
-        biggest = first >= second ?
-                first:
-                second;
-        System.out.println(biggest);
+        int result = 0;
+        result = first >= second ? 10: -10;
+        System.out.println(result);
     }
+
 }
